13bd8b0f0addb2839f6bc3761cf46967a9132ca8
xinyu <jolin7180732@gmail.com>
2024-01-10 13:13:42 +0800

    fix week2

--- week2/login.js
+++ week2/login.js
@@ -1,28 +1,37 @@
-const url = "https://vue3-course-api.hexschool.io/v2"; // 請加入站點
-const path = "lucky_herb_spices"; // 請加入個人 API Path
+// 具名引用
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
-let emailInput = document.querySelector("#username");
-let passwordInput = document.querySelector("#password");
-let loginBtn = document.querySelector("#login");
+//Options API
+createApp({
+  data() {
+    return {
+      //API path
+      url: "https://vue3-course-api.hexschool.io/v2", // 請加入站點
+      path: "lucky_herb_spices", // 請加入個人 API Path
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // console.log(this.username, this.password);
+      let username = this.username;
+      let password = this.password;
+      let user = { username, password };
 
-loginBtn.addEventListener("click", (e) => {
-  //   console.log(emailInput.value);
-
-  let username = emailInput.value;
-  let password = passwordInput.value;
-  let user = { username, password };
-
-  axios
-    .post(`${url}/admin/signin`, user)
-    .then((res) => {
-      // console.log(res);
-      const { token, expired } = res.data;
-      // console.log(token, expired);
-      document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
-      window.location.replace("products.html");
-    })
-    .catch((err) => {
-      console.dir(err);
-      alert(err.data.message);
-    });
-});
+      axios
+        .post(`${this.url}/admin/signin`, user)
+        .then((res) => {
+          // console.log(res);
+          const { token, expired } = res.data;
+          // console.log(token, expired);
+          document.cookie = `hexToken=${token};expires=${new Date(expired)}`;
+          window.location.replace("products.html");
+        })
+        .catch((err) => {
+          console.dir(err);
+          alert(err.data.message);
+        });
+    },
+  },
+}).mount("#app");
