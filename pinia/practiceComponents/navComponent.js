b8b2affcad1aafd07b2fa41c6d302c9c581cec7c
xinyu <jolin7180732@gmail.com>
2024-01-23 19:02:49 +0800

    pinia store

---
+++ pinia/practiceComponents/navComponent.js
@@ -0,0 +1,11 @@
+export default {
+  template: `<nav class="navbar bg-light">
+    <div class="container-fluid">
+      <span class="navbar-brand" href="#">香香麵攤</span>
+      <div>
+        購物車
+        <span class="badge rounded-pill bg-danger text-white"> 1 </span>
+      </div>
+    </div>
+  </nav>`,
+};
