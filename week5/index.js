3dd358938a99420a9372ed9bd7240a21fcff9f36
xinyu <jolin7180732@gmail.com>
2024-01-28 22:23:06 +0800

    week 5 add to cart

--- week5/index.js
+++ week5/index.js
@@ -1,5 +1,7 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+let detailModal = "";
+
 const app = createApp({
   data() {
     return {
@@ -7,6 +9,8 @@ const app = createApp({
       path: "lucky_herb_spices",
       pagination: {},
       products: [],
+      productDetail: {},
+      cart: { qty: 1 },
     };
   },
   methods: {
@@ -14,17 +18,52 @@ const app = createApp({
       axios
         .get(`${this.url}/api/${this.path}/products`)
         .then((res) => {
-          console.log(res.data);
+          // console.log(res.data);
           this.pagination = res.data.pagination;
           this.products = res.data.products;
         })
         .catch((err) => {
-          console.dir(err);
+          // console.dir(err);
+          alert(err.data.message);
+        });
+    },
+    getProductDetail(id) {
+      // console.log(id);
+      axios
+        .get(`${this.url}/api/${this.path}/product/${id}`)
+        .then((res) => {
+          // console.log(res.data);
+          detailModal.show();
+          this.productDetail = res.data.product;
+        })
+        .catch((err) => {
+          // console.dir(err);
+          alert(err.data.message);
+        });
+    },
+    addToCart(product_id) {
+      // 傳入資料格式
+      // "data": {
+      //   "product_id": "-L9tH8jxVb2Ka_DYPwng",
+      //   "qty": 1
+      // }
+      // console.log(product_id, this.cart.qty);
+      this.cart = { product_id, ...this.cart };
+
+      axios
+        .post(`${this.url}/api/${this.path}/cart`, { data: this.cart })
+        .then((res) => {
+          // console.log(res.data);
+          alert(res.data.message);
+        })
+        .catch((err) => {
+          // console.dir(err);
           alert(err.data.message);
         });
     },
   },
   mounted() {
     this.getProduct();
+    detailModal = new bootstrap.Modal(this.$refs.modal);
   },
 }).mount("#app");
